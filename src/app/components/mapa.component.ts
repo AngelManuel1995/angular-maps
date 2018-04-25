88352736f8182b7cbfd5627570abed62225aac47
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-24 21:37:11 -0500

    adding marker with localStorage and snackBar

--- src/app/components/mapa.component.ts
+++ src/app/components/mapa.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core' 
 import { Marker } from '../classes/marcador.class' 
+import { MatSnackBar } from '@angular/material'
 @Component({
 	selector:'app-mapa',
 	templateUrl:'./mapa.component.html'
@@ -13,15 +14,36 @@ export class MapaComponent{
 	lng:number = -76.02611 
 	zoom:number = 16 
 
-	constructor(){
-		let marcador = new Marker(6.750597,-76.02611) 
-		this.markers.push(marcador) 
+	constructor(private snackBar:MatSnackBar){
+		let marker = new Marker(6.750597,-76.02611) 
+		this.markers.push(marker) 
+		this.getLocalStorageData()
+
 	}
 
 	addMarker( event ){
 		let coords:{lat:number, lng:number} = event.coords
 		let marker = new Marker(coords.lat, coords.lng)
 		this.markers.push(marker)
+		this.updateLocalStorageData()
+		this.snackBar.open('Marcador agregado', 'Cerrar', {duration:1500})
+	}
+
+	updateLocalStorageData(){
+		localStorage.setItem('markers', JSON.stringify(this.markers))
+	}
+	
+	getLocalStorageData(){
+		if(localStorage.getItem('markers')){
+			return this.markers = JSON.parse(localStorage.getItem('markers'))
+		}
+		return console.log('No hay marcadores');
+	}
+
+	deleteMarker(i){
+		this.markers.splice(i,1)
+		this.updateLocalStorageData()
+		this.snackBar.open('Marcador eliminado', 'Cerrar', {duration:1500})				
 	}
 
 }
